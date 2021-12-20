29d80cc16e4115303441591e4bebad8c79c884cd
Will Firman <w.f@quantumcryogenics.com>
2021-12-20 11:42:17 +0000

    Allow user to choose between using nmcli or netplan for the connection switch

--- README.md
+++ README.md
@@ -4,29 +4,11 @@ to it.
 
 A function is provided with the below signature:
 ```python
-connect_to_network(ssid, password, path_to_netplan_yaml)
+connect_to_network(ssid, password, path_to_netplan_yaml=None)
 ```
 
-Executing this function will:
-- Look for a file at `path_to_netplan_yaml` and parse the YAML inside it.
-- Overwrite the `network.wifis.wlan0.access-points` node with the `ssid` and `password`
-provided. Any existing SSIDs and passwords will be replaced, not added to.
-- If the file doesn't contain the nested key `network.wifis.wlan0`, then it will be
-created and assigned the default values below (`dhcp4: true` and basic settings for the
-`ssid` and `password`)
-- If the file doesn't exist at all, then it will be created (as well as creating the full
-folder path), with the settings below.
-
-```yaml
-network:
-  version: 2
-  wifis:
-    wlan0:
-      access-points:
-        your_ssid:
-          password: your_password
-      dhcp4: true
-```
+If the `path_to_netplan_yaml` argument is provided, then `netplan` will be used to connect to the
+network. Otherwise, `nmcli` is used.
 
 ## Installation:
 
@@ -42,15 +24,55 @@ import logging
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 
-# All-in-one function to connect to a network
+# All-in-one function to connect to a network using netplan:
 connect_to_network('ssid', 'password', '/full/path_to/netplan_yaml_file.yaml')
 
-# Alternatively, just update the netplan yaml file:
-from wifi_switcher.wifi_switcher import update_netplan_settings
-update_netplan_settings('ssid', 'password', 'full_path_to_netplan_yaml_file')
-
-# Then generate & apply the changes (using `netplan generate` then `netplan apply`):
-from wifi_switcher.wifi_switcher import generate_and_apply_netplan_changes
-generate_and_apply_netplan_changes()
+# Alternatively, using nmcli:
+success, message = connect_to_network('ssid', 'password')
+if success:
+    # Do something when the connection attempt succeeds
+    pass
+else:
+    # Do something when the connection attempt fails
+    pass
 ```
+
+## Using netplan
+If you provide a value for the `path_to_netplan_yaml` argument, then the module will attempt to
+connect to a network using netplan.
+
+Executing this function will:
+- Look for a file at `path_to_netplan_yaml` and parse the YAML inside it.
+- Overwrite the `network.wifis.wlan0.access-points` node with the `ssid` and `password`
+provided. Any existing SSIDs and passwords will be replaced, not added to.
+- If the file doesn't contain the nested key `network.wifis.wlan0`, then it will be
+created and assigned the default values below (`dhcp4: true` and basic settings for the
+`ssid` and `password`)
+- If the file doesn't exist at all, then it will be created (as well as creating the full
+folder path), with the settings below.
+- The function returns a 2-tuple, to match the signature used when connecting via `nmcli`.
+However, it will always return `False` as the first argument because currently we're not able
+to check whether a connection attempt succeeded when using netplan.
+
+```yaml
+network:
+  version: 2
+  wifis:
+    wlan0:
+      access-points:
+        your_ssid:
+          password: your_password
+      dhcp4: true
+```
+
+## Using nmcli
+If you don't provide an argument for `path_to_netplan_yaml`, then the module will attempt to connect
+using nmcli.
+
+If the requested SSID is not in the list of available networks, then a network scan will be performed to
+try to find it and then the connection attempt will be repeated.
+
+The function returns a 2-tuple where the first element is a 
+boolean showing whether the connection attempt succeeded, and the second is a string containing the
+success/error message.
  
---
+++ src/wifi_switcher/netplan_tools.py
@@ -0,0 +1,131 @@
+import yaml
+import os
+import subprocess
+import logging
+
+from .exceptions import ExitStatusException
+
+logger = logging.getLogger()
+
+
+class NetplanYamlUpdater:
+    _default_wlan0 = {
+        'access-points': {},
+        'dhcp4': True
+    }
+
+    def __init__(self, path_to_netplan_file):
+        self.path_to_netplan_file = path_to_netplan_file
+        self.netplan_settings = None
+        self._load_netplan_settings()
+
+    def _load_netplan_settings(self):
+        """Opens the YAML file at self.path_to_netplan_file and parses it using PyYAML.
+
+        If the file is not found, instead returns an empty dict. If the file is found but is parsed into an object other
+        than a dict, a TypeError is raised.
+
+        self.netplan_settings will be set to the returned dict.
+
+        :return: The loaded yaml file, if it is found. Otherwise, an empty dict.
+        :rtype: dict
+        :raises: TypeError
+        """
+        try:
+            with open(self.path_to_netplan_file, 'r') as f:
+                data = f.read()
+                logger.debug(f'Opened existing netplan settings at {self.path_to_netplan_file}')
+        except FileNotFoundError:
+            data = None
+            logger.debug(f'No netplan settings found at {self.path_to_netplan_file}')
+
+        if data:
+            parsed_data = yaml.safe_load(data)
+            logger.debug(f'Parsed netplan settings: {parsed_data}')
+            if isinstance(parsed_data, dict):
+                self.netplan_settings = parsed_data
+                logger.info(f'Successfully opened & parsed netplan settings at {self.path_to_netplan_file}')
+            else:
+                raise TypeError(f'The YAML file at {self.path_to_netplan_file} does not parse into a Python dictionary,'
+                                f' so it cannot be loaded. The file parsed as: {type(parsed_data)}')
+
+        else:
+            self.netplan_settings = {}
+            logger.info(f'No existing netplan settings found at {self.path_to_netplan_file}, starting from blank'
+                        f' settings.')
+
+        return self.netplan_settings
+
+    def _get_or_create_wlan0_settings(self):
+        """Gets the wlan0 settings from the netplan settings, or creates default settings if none exist.
+
+        Attempts to return self.netplan_settings['network']['wifis']['wlan0']. If any of those keys don't exist,
+        they will be created and a default value for wlan0 will be created:
+        {'wlan0': 'access-points': {}, 'dhcp4': True}
+
+        :return: The wlan0 settings from self.netplan_settings
+        :rtype: dict
+        """
+        if self.netplan_settings is None:
+            self._load_netplan_settings()
+        return self.netplan_settings.setdefault(
+            'network', {'version': 2}
+        ).setdefault(
+            'wifis', {}
+        ).setdefault(
+            'wlan0', self._default_wlan0
+        )
+
+    def set_wlan0_access_point(self, ssid, password):
+        """Replaces the current access point(s) under wlan0 with the provided ssid/password"""
+        access_points = self._get_or_create_wlan0_settings()['access-points']
+
+        for key in list(access_points.keys()):
+            access_points.pop(key)
+            logger.debug(f'Removed existing access point: {key}')
+
+        access_points[ssid] = {'password': password}
+        logger.info(f'Added access point to wlan0: {ssid}')
+
+        return access_points
+
+    def save(self):
+        """Dumps self.netplan_settings to a YAML file at the location in self.path_to_netplan_file"""
+        # Create the folder tree if it doesn't exist already
+        folder = os.path.dirname(self.path_to_netplan_file)
+        if folder:
+            os.makedirs(folder, exist_ok=True)
+            logger.debug(f'Folder did not exist, but was created: {folder}')
+
+        # Save the yaml file
+        with open(self.path_to_netplan_file, 'w') as f:
+            yaml.dump(self.netplan_settings, f)
+            logger.info(f'Saved new netplan settings to disk at {self.path_to_netplan_file}')
+
+
+def update_netplan_settings(ssid, password, path_to_netplan_yaml):
+    netplan = NetplanYamlUpdater(path_to_netplan_yaml)
+
+    netplan.set_wlan0_access_point(ssid, password)
+    netplan.save()
+
+
+def generate_and_apply_netplan_changes():
+    generate_command = ['netplan', 'generate']
+    generate_result = subprocess.run(generate_command, capture_output=True)
+    if generate_result.returncode != 0:
+        raise ExitStatusException(f'Non-zero exit status was generated from command {generate_command}.'
+                                  f' Exit status code: {generate_result.returncode}.\nError message:\n'
+                                  f'{generate_result.stderr.decode("utf-8")}')
+    else:
+        apply_command = ['netplan', 'apply']
+        apply_result = subprocess.run(apply_command, capture_output=True)
+        if apply_result.returncode != 0:
+            raise ExitStatusException(f'Non-zero exit status was generated from command {apply_command}.'
+                                      f' Exit status code: {apply_result.returncode}.\nError message:\n'
+                                      f'{apply_result.stderr.decode("utf-8")}')
+
+
+def connect_to_network_with_netplan(ssid, password, path_to_netplan_yaml):
+    update_netplan_settings(ssid, password, path_to_netplan_yaml)
+    generate_and_apply_netplan_changes()
